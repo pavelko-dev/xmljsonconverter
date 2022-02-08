0a85fda5b7662550a7e663d30cbec85016e0533c
Pavlo Pavlichenko <ICPavlo.Pavlichenko@AmericanWell.com>
2022-02-08 18:23:14 +0200

    first commit

---
+++ src/main/java/com/ppv/xmljsonconverter/XmljsonconverterApplication.java
@@ -0,0 +1,13 @@
+package com.ppv.xmljsonconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmljsonconverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmljsonconverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ppv/xmljsonconverter/XmljsonconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppv.xmljsonconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmljsonconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
